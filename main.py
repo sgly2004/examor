9eb21b43f65744adec9981f0ef7e3d0b688c7d05
Qiyuan Liu <166361927@qq.com>
2024-03-14 10:28:14 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from langchain_openai import ChatOpenAI
+
+def main():
+    # 初始化模型并提供秘钥
+    llm = ChatOpenAI(openai_api_key="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")
+
+    llm.invoke("how can langsmith help with testing?")
+
+if __name__ == "__main__":
+  main()
+
+
